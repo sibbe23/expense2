84e645835205d54531509a2e041d3024f30e0e9d
sibbe23 <sibbe23@gmail.com>
2023-04-13 12:16:58 +0530

    cd last pos 4

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,7 +10,7 @@ const authenticate = (req, res, next) => {
         // console.log('userID >>>> ', user.userId)
         User.findByPk(user.userId).then(user => {
 
-            req.user = user; ///ver
+            req.user = user; 
             next();
         })
 
