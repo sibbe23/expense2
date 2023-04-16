3caeabc2597b97695432c7bb680aa7a8bd7fbec5
sibbe23 <sibbe23@gmail.com>
2023-04-16 15:04:16 +0530

    minor change

--- util/database.js
+++ util/database.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB_SCHEMA_NAME,process.env.DB_ROOT_USER,process.env.DB_SCHEMA_PASS,{
     dialect: 'mysql',
-    host: process.env.DB_54.145.244.191,
+    host: process.env.DB_HOST,
 });
 module.exports = sequelize;
